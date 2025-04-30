b6bb7d5a87ac59def3bb25b57512ca8ef114fddd
Topher Brown <topher@memfault.com>
2025-04-30 02:17:23 -0400

    feat: rename podcast using iteration and current date

--- run-wafflebot.sh
+++ run-wafflebot.sh
@@ -17,11 +17,19 @@ echo "Running audio mixer..."
 docker compose run --rm audio-mixer
 
 echo "Uploading podcast..."
-cp data/podcast/* "/home/topher/Dropbox/Apps/PushPod/Haotic Waffles"
+OUTPUT_DIR="/home/topher/Dropbox/Apps/PushPod/Haotic Waffles"
+
+# find the latest file in the output directory
+LATEST_FILE=$(ls -t "$OUTPUT_DIR"/*.mp3 | head -n 1)
+
+# get its prefix (e.g. "1-April 30, 2025")
+PREFIX=$(basename "$LATEST_FILE" .mp3 | cut -d '-' -f 1)
+
+# rename our file to include that prefix, +1, plus today's date
+NEW_FILE="$PREFIX-$(date +%d).mp3"
+mv "$LATEST_FILE" "$OUTPUT_DIR/$NEW_FILE"
 
 echo "Cleaning up voice memos..."
 rm -rf data/voice-memos
-echo "Cleaning up generated podcast..."
-rm -rf data/podcast
 
 echo "Done!"
